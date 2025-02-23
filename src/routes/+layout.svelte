75cff18878210102a7989ede6a8f0517d9f7033e
Richard Miskin II <phigamgentleman@icloud.com>
2025-02-23 14:52:46 -0500

    added vercel speed-insights for sveltekit

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,9 @@
 <script>
 	import Header from './Header.svelte';
 	import '../app.css';
+	import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
 
+	injectSpeedInsights();
 	/** @type {{children: import('svelte').Snippet}} */
 	let { children } = $props();
 </script>
