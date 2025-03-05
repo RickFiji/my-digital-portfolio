f747533ffbb74f56e396c9645a11f6bc34f21e97
Richard Miskin II <phigamgentleman@icloud.com>
2025-03-05 18:30:20 -0500

    added lib/images to the compiler path.

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,13 +1,17 @@
 // See https://svelte.dev/docs/kit/types#app.d.ts
 // for information about these interfaces
 declare global {
-	namespace App {
-		// interface Error {}
-		// interface Locals {}
-		// interface PageData {}
-		// interface PageState {}
-		// interface Platform {}
-	}
+    namespace App {
+        // interface Error {}
+        // interface Locals {}
+        // interface PageData {}
+        // interface PageState {}
+        // interface Platform {}
+    }
 }
-
 export {};
+
+declare module '*.svg' {
+    const content: string;
+    export default content;
+}
